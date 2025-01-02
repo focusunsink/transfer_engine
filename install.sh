e1cb493347ce28139e31a39be37c024eb73b7845
focusunsink <549303468@qq.com>
2025-01-02 17:05:57 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+# https://developer.nvidia.com/nccl/nccl-download
 distribution=$(. /etc/os-release; echo $ID$VERSION_ID) && curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | sudo apt-key add - && curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list | sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
 apt-get update
 apt-get install -y libnccl2 libnccl-dev
